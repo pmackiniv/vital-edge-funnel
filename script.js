b729d055e63f5ecb70e2c3c8a274f12092ed94f5
pmackiniv <162835457+pmackiniv@users.noreply.github.com>
2025-12-08 23:52:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function checkConsent(e) {
+  const box = document.getElementById("consent-checkbox");
+  if (!box.checked) {
+    e.preventDefault();
+    alert("Please confirm your consent before continuing.");
+    return false;
+  }
+}
