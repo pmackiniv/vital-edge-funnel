7f0d9f87b579fc92a01e835c1f512725cb0e6b78
pmackiniv <162835457+pmackiniv@users.noreply.github.com>
2025-12-09 00:10:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function checkConsent(e) {
-  const box = document.getElementById("consent-checkbox");
-  if (!box.checked) {
-    e.preventDefault();
-    alert("Please confirm your consent before continuing.");
-    return false;
-  }
-}
